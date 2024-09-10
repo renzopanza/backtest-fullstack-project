6b8647446343820ba0e2cb62f3ebb3cd8b5e0556
Lucas Ciampi <lucas_ciampi@hotmail.com>
2024-09-10 17:10:47 -0300

    Criação do Menu Principal

---
+++ frontend/Front/src/components/MenuPrincipal.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import "./MenuPrincipal.css"
+
+const MenuPrincipal = () => {
+
+    return (
+        <div></div>
+    );
+};
+
+export default MenuPrincipal;
