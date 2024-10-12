a4ddea01bc8819a305010c020c7c062130cbdca3
Lucas Ciampi <lucas_ciampi@hotmail.com>
2024-10-11 21:11:16 -0300

    Mudanças no layout do front

--- frontend/src/components/MenuPrincipal/MenuPrincipal.jsx
+++ frontend/src/components/MenuPrincipal/MenuPrincipal.jsx
@@ -8,13 +8,13 @@ const MenuPrincipal = () => {
             <h2 className='Titulo'>Digite abaixo as informacoes da opcao que deseja pesquisar:</h2>
             <div className='Digitacao'>
                 <div className='Local_digitacao_nome'>
-                    <h3>Nome da opcao:</h3>
+                    <h3>Nome da opção:</h3>
                     <input type="number" className='Input' />
                 </div>
                 <div className='Local_digitacao_data'>
-                    <h3>Intervalo de datas das opcoes:</h3>
-                    <h5>Inicio:<input type="date" className='Input_data' /></h5>
-                    <h5>Fim<input type="date" className='Input_data' /></h5>
+                    <h3>Intervalo de datas das opções:</h3>
+                    <h5 className='h5'>Inicio:<input type="date" className='Input_data' /></h5>
+                    <h5 className='h5'>Fim:<input type="date" className='Input_data' /></h5>
                 </div>
             </div>
         </div>
